90b46eec832d6505dbba80dbe0653ef7bbd0fe03
LeDinhPhuc <ledinhphucts98@gmail.com>
2020-11-30 14:06:38 +0700

    deploy

---
+++ build/precache-manifest.6a2e6727fa0c8ff5c93f66cf35bd82b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adb2d3d280ab88aaa8e91cdbdb882d5c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28b59954241643749818",
+    "url": "/static/css/2.cb16d82a.chunk.css"
+  },
+  {
+    "revision": "79daced88ae0abf50292",
+    "url": "/static/css/main.38456174.chunk.css"
+  },
+  {
+    "revision": "28b59954241643749818",
+    "url": "/static/js/2.8c6718ed.chunk.js"
+  },
+  {
+    "revision": "634e66e16b27d5808c041ffc8e87166a",
+    "url": "/static/js/2.8c6718ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79daced88ae0abf50292",
+    "url": "/static/js/main.f9c5724b.chunk.js"
+  },
+  {
+    "revision": "55957f1e5a87a5450121",
+    "url": "/static/js/runtime-main.38aca6d9.js"
+  }
+]);
