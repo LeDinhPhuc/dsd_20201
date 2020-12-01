e69c2effb8df1862d43d5f65ea55d590fcf663ec
LeDinhPhuc <ledinhphucts98@gmail.com>
2020-12-01 21:26:37 +0700

    edit app.js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const HOST = process.env.HOST || '0.0.0.0';
 const PORT = 9999;
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-app.listen(PORT, HOST, console.log(`Server is starting at port ${PORT}... `));
+
+app.listen(PORT, console.log(`Server is starting at port ${PORT}...`));
